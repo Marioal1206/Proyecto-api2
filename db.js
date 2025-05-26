8df9727495f5854e525daeecbc6cb140b9f02d70
MarioRodriguez <l20100257@nlaredo.tecnm.mx>
2025-05-26 06:04:33 -0500

    cambios
    cambios

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ const conexion = mysql.createConnection({
  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 conexion.connect((err) => {
